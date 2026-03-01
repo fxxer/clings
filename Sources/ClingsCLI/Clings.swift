791ce0c7c8b1ec12b629c549beb203b9dc965f9c
Michal Moravčík <michal.moravcik@me.com>
2026-03-01 14:19:37 +0100

    Bump version to 0.3.0

--- Sources/ClingsCLI/Clings.swift
+++ Sources/ClingsCLI/Clings.swift
@@ -27,7 +27,7 @@ struct Clings: AsyncParsableCommand {
         For more information on a specific command, run:
           clings <command> --help
         """,
-        version: "0.2.10",
+        version: "0.3.0",
         subcommands: [
             // List views
             TodayCommand.self,
